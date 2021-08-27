5fd0a329f04e5eed0e0e4f93861f44fe33f82bd0
Sergey <sergey@terentiev.me>
2021-08-27 16:03:58 +0300

    anychart

--- websock.py
+++ websock.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 
-# WS server that sends messages at random intervals
-
 import asyncio
 import datetime
 import random
 import websockets
-
 import serial
-
-
-
 
 async def time(websocket, path):
     print("Start serial")
     ser = serial.Serial('/dev/ttyACM0',timeout=0.5)  # open serial port
 
     while True:
-        
         s = ser.readline()
         if len(s) != 0:
             d = str(s,'utf-8')
@@ -26,12 +19,8 @@ async def time(websocket, path):
             count = int(serial_data[1])
             adc = int(serial_data[2])
             distance = 0
-            print(adc)
             await websocket.send('{"count": '+str(count)+', "adc": '+str(adc)+', "distance": '+str(distance)+'  }')
         
-        #await asyncio.sleep(random.random() * 3)
-
 start_server = websockets.serve(time, "127.0.0.1", 5678)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
